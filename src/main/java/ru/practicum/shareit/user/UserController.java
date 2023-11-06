290ed616ebb582d47019eb1a4f80db59b50b1305
Sergey <moskovets2319@yandex.ru>
2023-11-06 23:43:30 +0300

    рефакторинг кода, проверка наличия пользователя при обновлении сделана в сервисном слое

--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -46,7 +46,7 @@ public class UserController {
 
     @DeleteMapping("/{userId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteUser(@PathVariable("userId") Long userId) {
+    public void deleteUser(@PathVariable Long userId) {
         userService.deleteUser(userId);
     }
 
